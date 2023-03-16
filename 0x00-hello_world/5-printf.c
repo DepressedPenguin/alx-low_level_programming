f18060ce8923ddc9d5e05c684eb5c752f8ef23c3
ZAKARIA <zakariaelaro@gmail.com>
2023-03-16 10:09:12 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using the -Wall gcc option
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
