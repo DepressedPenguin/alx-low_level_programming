c20d771b81c4f152c139452dd17e21015aea23af
ZAKARIA <zakariaelaro@gmail.com>
2023-03-16 10:03:34 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 •#include <stdio.h>
 /**
  * main - Prints a message to the console
- * Return: 0 (Success)
+ * Return: (Success)
  */
 int main(void)
 {
 /*Puts is designed to print strings to the console*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}•
+}
