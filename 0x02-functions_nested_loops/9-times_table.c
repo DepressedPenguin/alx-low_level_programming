24f9cb75ed7a24a3c2cff82b18aaddb93a4d0e6c
ZAKARIA <zakariaelaro@gmail.com>
2023-03-21 13:44:00 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,32 @@
  */
 void times_table(void)
 {
-	int i, j, product;
+int i, j, product;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+product = i * j;
 
-			if (j != 0)
-				_putchar(',');
-			if (product < 10 && j != 0)
-				_putchar(' ');
-else
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+
+if (product < 10 && j != 0)
+{
+_putchar(' ');
+}
+
+if (product >= 10)
+{
 _putchar(product / 10 + '0');
-_putchar(product % 10 + '0');
+}
 
+_putchar(product % 10 + '0');
 }
 _putchar('\n');
 }
 }
-
