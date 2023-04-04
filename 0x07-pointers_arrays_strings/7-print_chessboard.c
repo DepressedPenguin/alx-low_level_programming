39e21d96c7134984d80e226abb157df71149d697
ZAKARIA <zakariaelaro@gmail.com>
2023-04-04 09:36:05 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @(*a)[8]: This is a pointer to a two-dimensional //Zakaria Elaroussi
+ * @a: is a pointer to an array of 8 characters
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int n, b;
+for (n = 0; n < 8; n++)
+{
+for (b = 0; b < 8; b++)
+{
+_putchar(a[n][b]);
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
