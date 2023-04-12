232f626245f9343d4d6b88d5ed88489b244d8fac
ZAKARIA <zakariaelaro@gmail.com>
2023-04-11 19:46:34 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
+char *str_concat(char *s1, char *s2);
 int _putchar(char c);
 
 #endif /* MAIN_H Zakaria Elaroussi */
