1a42e67831f3ed9518013de0fb0f0a00c9737449
ZAKARIA <zakariaelaro@gmail.com>
2023-04-17 10:36:58 -0700

    Poppy Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+/**
+ * struct dog - Define a new type vars
+ * @name: name char dog
+ * @age: age float dog
+ * @owner: owner char dog
+ */
+
+struct dog
+{
+char  *name;
+float  age;
+char  *owner;
+};
+#endif
