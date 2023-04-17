1b91fdbcd687da4beda6253ffb662e5b88375e87
ZAKARIA <zakariaelaro@gmail.com>
2023-04-17 07:00:02 -0700

    File name task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file
+ * __FILE__: Predefined Macro
+ * Return: nothing
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
