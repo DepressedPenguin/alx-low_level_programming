c6892b06bf7fcf0af6ccd7c55767a1ba111423ec
ZAKARIA <zakariaelaro@gmail.com>
2023-04-27 06:57:23 -0700

    Task 0 Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 int c = 0;
-for (c = 0 ;h != NULL ; c++)
+for (c = 0 ; h != NULL ; c++)
 {
 if (h->str != NULL)
 printf("[%u] %s\n", h->len, h->str);
