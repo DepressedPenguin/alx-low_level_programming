4c4ad2c339f72c148937d788f7ab1541406a462a
ZAKARIA <zakariaelaro@gmail.com>
2023-05-04 12:28:15 -0700

    Task 5 101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int num1, unsigned long int num2)
+{
+unsigned int counts = 0;
+unsigned long int xor_result = num1 ^ num2;
+    
+while (xor_result)
+{
+counts += xor_result & 1;
+xor_result >>= 1;
+}
+    
+return (counts);
+}
