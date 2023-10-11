cf6cd814b08442b75bff7a352226755c71f208a7
ZAKARIA <zakariaelaro@gmail.com>
2023-10-11 03:22:41 -0700

    Trying To fix code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * linear_search - Searches for a value in an array
@@ -13,17 +14,20 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-    if (array == NULL) {
-        return -1;
+    if (array == NULL) 
+    {
+        return (-1);
     }
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) 
+    {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        if (array[i] == value) {
-            return i;
+        if (array[i] == value) 
+        {
+            return (i);
         }
     }
 
-    return -1;
+    return (-1);
 }
