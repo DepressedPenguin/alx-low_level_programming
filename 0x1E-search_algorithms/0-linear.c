3fb946fdd437be886e5fd630ed267452a8230577
ZAKARIA <zakariaelaro@gmail.com>
2023-10-11 03:15:20 -0700

    Task 0 check

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * linear_search - Searches for a value in an array
+ *
+ * @array: Pointer to the first element
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (array == NULL) {
+        return -1;
+    }
+
+    for (i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+        if (array[i] == value) {
+            return i;
+        }
+    }
+
+    return -1;
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+<h1>0x1E-search_algorithms</h1>
+TASK 0X1E
